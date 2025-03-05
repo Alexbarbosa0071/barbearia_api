87dd6b9a70e58130027a5715c66f0d901b008595
Alex Sandro <alexsandro0071@hotmail.com>
2025-03-05 03:54:00 -0300

    Ajustando cors

--- BarbeariaApi/Program.cs
+++ BarbeariaApi/Program.cs
@@ -3,7 +3,19 @@ using BarbeariaApi.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.AddCollectionExtensions();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        name: "AllowOrigin",
+        builder => {
+            builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+        });
+});
 var app = builder.Build();
+app.UseCors("AllowOrigin");
 
 app.MapClienteEndpoints();
 
